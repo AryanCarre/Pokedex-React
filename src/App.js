c0dba04accfa1dc0b3b2efbc38cba2b3a7d8427c
AryanCarre <aryancarre@outlook.com>
2023-01-11 17:29:47 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/Navbar";
 import SearchBar from "./components/Searchbar";
+import { searchPokemon } from "./api";
 
+const onSearchHandler = async (pokemon) => {
+  const result = await searchPokemon(pokemon)
+  console.log(result)
+}
 function App() {
   return (
     <div>
       <NavBar />
-      <SearchBar />
+      <SearchBar 
+        onSearch={onSearchHandler}
+      />
       <div className="App">
       </div>
     </div>
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,16 +1,22 @@
 import React, {useState} from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+    const {onSearch} = props
     const [search, setSearch] = useState("ditto") 
 
-    const onChabgeHandler = (e) => {
-        search = e.target.value
+    const onChangeHandler = (e) => {
+        setSearch(e.target.value)
+    }
+    const onButtonClickHandler = () => {
+        onSearch(search)
     }
     return (
         <div className="searchbar-container">
             <div className="searchbar">
-                <input placeholder="Buscar Pokemon" onChange={onChabgeHandler}/>
-                {search}
+                <input placeholder="Buscar Pokemon" onChange={onChangeHandler}/>
+            </div>
+            <div className="searchbar-btn">
+                <button onClick={onButtonClickHandler}>Buscar</button>
             </div>
         </div>
     )
